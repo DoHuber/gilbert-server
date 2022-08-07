bcadb6880f372963ffa1efc812c60ba7aff0a3e6
Dominik Huber <elcpt@elcpt.de>
2022-08-07 19:39:10 +0200

    Refactor token auth

--- src/main/java/GilbertServer.java
+++ src/main/java/GilbertServer.java
@@ -1,10 +1,23 @@
+import spark.Request;
+
 import static spark.Spark.*;
 
 public class GilbertServer {
 
     private final static String authToken = "abcdef";
 
+    private static void checkAuth(Request req) {
+        String suppliedToken = req.queryParams("token");
+        if (!authToken.equals(suppliedToken)) {
+            halt(401, "Bad token.");
+        }
+    }
+
     public static void main(String[] args) {
+
+        before((req, response) -> {
+            checkAuth(req);
+        });
 
         get("/hello", (req, res) -> {
 
